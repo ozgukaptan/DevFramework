30b29baa7af11a0f15e98038c806709b9dab9fdb
ozgu kaptan <ozgukp@hotmail.com>
2020-05-11 00:58:30 +0300

    validationTool

---
+++ DevFramework.Nort.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace DevFramework.Nort.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTool
+    {
+        public static void FuluentValidate(IValidator validator, object entity)
+        {
+            var result = validator.Validate(entity);
+
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
