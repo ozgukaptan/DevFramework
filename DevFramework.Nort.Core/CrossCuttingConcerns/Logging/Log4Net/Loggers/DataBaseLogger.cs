3db01cf9d9afacee6b167eceaeff06ae36fb4359
ozgu kaptan <ozgukp@hotmail.com>
2020-05-23 02:56:24 +0300

    Log2

---
+++ DevFramework.Nort.Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/DataBaseLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace DevFramework.Nort.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class DataBaseLogger : LoggerService
+    {
+        public DataBaseLogger() : base(LogManager.GetLogger("DataBaseLogger"))
+        {
+        }
+    }
+}
--- DevFramework.Nort.Core/CrossCuttingConcerns/Logging/Log4Net/SerializableLogEvent.cs
+++ DevFramework.Nort.Core/CrossCuttingConcerns/Logging/Log4Net/SerializableLogEvent.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net.Core;
 
 namespace DevFramework.Nort.Core.CrossCuttingConcerns.Logging.Log4Net
 {
-    class SerializableLogEvent
+    [Serializable]
+    public class SerializableLogEvent
     {
+        private LoggingEvent _loggingEvent;
+
+        public SerializableLogEvent(LoggingEvent loggingEvent)
+        {
+            _loggingEvent = loggingEvent;
+        }
+
+        public string UserName => _loggingEvent.UserName;
+        public object MessageObject => _loggingEvent.MessageObject;
+
     }
 }
